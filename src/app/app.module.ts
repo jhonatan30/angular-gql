5327b42d64310fc2dbe279c8a815c449bf971a20
Jhonatan S. Delgado <JhDelgado@wfscorp.com>
2019-09-04 15:19:15 -0500

    Implement dran dn drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
@@ -13,7 +15,9 @@ import { UsersListComponent } from './components/users-list/users-list.component
   ],
   imports: [
     AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
+    DragDropModule,
     GraphQLModule,
     HttpClientModule
   ],
--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag';
 import { Apollo } from 'apollo-angular';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { newUserConverter } from 'src/app/decorators/new-user-converter';
@@ -16,13 +17,22 @@ export class UsersListComponent implements OnInit {
   @newUserConverter
   isNew: boolean = true;
 
-  users: Observable<User[]>;
+  // users: Observable<User[]>;
+  users: User[] = [{
+    id: 0,
+    userId: 0,
+    isNew: false,
+    title: '',
+    body: ''
+  }];
 
+  alterUsers: User[] = [];
   constructor(private apollo: Apollo) { }
 
   ngOnInit() {
 
-    this.users = this.apollo.watchQuery<Query>({
+    // this.users = this.apollo.watchQuery<Query>({
+    this.apollo.watchQuery<Query>({
       query: gql`
         query allUsers {
           allUsers {
@@ -33,10 +43,19 @@ export class UsersListComponent implements OnInit {
             body
           }
         }`
-    }).valueChanges.pipe(
-      map((result: any) => result.data.allUsers)
-    );
+    }).valueChanges.subscribe(response => this.users = response.data.allUsers);
+    // }).valueChanges.pipe(
+    //   map((result: any) => result.data.allUsers)
+    // );
+  }
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer !== event.container) {
+      transferArrayItem(event.previousContainer.data, event.container.data,
+        event.previousIndex, event.currentIndex);
+    } else {
+      moveItemInArray(this.users, event.previousIndex, event.currentIndex);
+    }
   }
 
 }
